1d68e561742d3a5625c37e6c064c452a990a0b9a
Hearth111 <97719296+Hearth111@users.noreply.github.com>
2025-07-29 10:24:42 +0900

    Add site settings page and sidebar link

---
+++ osarebito-frontend/src/app/site-settings/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+export default function SiteSettings() {
+  return (
+    <div className="max-w-md mx-auto mt-10">
+      <h1 className="text-2xl font-bold mb-4">サイト設定</h1>
+      {/* TODO: settings form */}
+    </div>
+  )
+}
